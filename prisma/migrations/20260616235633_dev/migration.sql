5eb5601838524a43fd3b7e8f65c0112715323982
suil0304 <eagle030410@gmail.com>
2026-06-19 10:00:40 +0900

    feat: initialize database schema with User, Stats, and Upgrade tables

--- prisma/migrations/20260616235633_dev/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `user` MODIFY `nickname` VARCHAR(191) NULL;
--- prisma/migrations/20260619010010_init/migration.sql
+++ prisma/migrations/20260619010010_init/migration.sql
@@ -2,11 +2,12 @@
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` CHAR(10) NOT NULL,
-    `password` CHAR(22) NOT NULL,
-    `nickname` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `nickname` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `User_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
