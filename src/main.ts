731fb2a6801bab4d43d8157355883c65d1e417b3
suil0304 <eagle030410@gmail.com>
2026-06-21 16:16:03 +0900

    feat: update CORS configuration to use FRONTEND_URL from environment variables

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env["FRONTEND_URL"],
+    credentials: true
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
